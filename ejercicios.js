1d0584788ccfcaaec84387591eca95ff83c54991
Paula <paula_reiche@hotmail.com>
2022-03-24 17:07:23 -0300

    ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,56 @@
+//Segundo ejercicio
+let num = 0
+num = 3;
+let numAlCubo = (num * num) * num;
+console.log('El resultado es: ' + numAlCubo);
+
+//Tercer ejercicio
+let año = 0;
+let añoDeNacimiento = 0;
+año = 2022
+añoDeNacimiento = 1996;
+let edad = año - añoDeNacimiento;
+console.log('Su edad es: ' + edad);
+
+//Cuarto ejercicio
+let distanciaCiudades = 0;
+let velocidadPromedio = 0;
+distanciaCiudades = 10;
+velocidadPromedio = 5;
+console.log('El tiempo aproximado de su llegada es de: ' + distanciaCiudades/velocidadPromedio + 'hs');
+
+//Quinto ejercicio
+let sueldoFijo = 15000;
+let totalFacturado = 100;
+console.log(`Su sueldo total es de: $${sueldoFijo+(totalFacturado*0.05)}`);
+
+//Sexto ejercicio
+let primeraNota = 0;
+let segundaNota = 0;
+let terceraNota = 0;
+primeraNota = 3;
+segundaNota = 9;
+terceraNota = 3;
+console.log('Su promedio es de:',(primeraNota+segundaNota+terceraNota)/3);
+
+//Septimo ejercicio
+let totalPredio = 0;
+let ocupadoPredio = 0;
+let porcentajeTotal = (totalFacturado / 100)*ocupadoPredio;
+console.log('El porcentaje cubierto es de: ' + porcentajeTotal + '%, queda disponible: ' + (100-porcentajeTotal) + '%');
+
+//Octavo ejercicio
+let valorDeLaCompra = 0;
+valorDeLaCompra = 100;
+console.log(`El valor total de su compra es de: $${valorDeLaCompra*0,85}`);
+
+//Noveno ejercicio
+let hombres = 70;
+let mujeres = 30;
+let totalDeAlumnos = (hombres + mujeres)/100;
+console.log('El porcentaje de mujeres en la carrera de ciencias exactas es de: ' + (totalDeAlumnos*mujeres) + '% y el de los hombres es de: ' + (totalDeAlumnos*hombres) + '%');
+ 
+//Decimo ejercicio
+let num1 = 4;
+let num2 = 2;
+console.log('Sus resultados son: sumados ' + (num1+num2) + ', restados ' + (num1-num2) + ', multiplicados ' + (num1*num2) + ' y divididos ' + (num1/num2));
